5385ecd30ea95f63dc20325d3af0e47a8e17a190
JimStockman <JimStockman@users.noreply.github.com>
2020-01-23 15:14:26 +0100

    wub

---
+++ wub/wub.py
@@ -0,0 +1,7 @@
+def song_decoder(song):
+    return " ".join(c for c in song.split('WUB') if c != '').strip()
+
+
+# I had this solution, but for some reason it was crashing on the web interpreter
+def best_song_decoder(song):
+  return " ".join(song.replace('WUB', ' ').split())
